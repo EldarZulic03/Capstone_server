332183cf51ad1badc07a69eac7493c90e103ca14
hassank1187 <hassanmkhan2000@gmail.com>
2022-11-09 15:15:12 -0500

    get rid of stuff that uses pyaudio for now

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import os
 from flask import Flask, render_template, request, session, redirect, Response
 from flask_session import Session
 import shutil
-import pyaudio
 import cv2
 import pickle5 as pickle
 import people_manager as pm
@@ -89,101 +88,6 @@ def train_models(patient,loved_one):
 #create main app
 app = Flask(__name__)
 api = Api(app)
-
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-CHUNK = 1024
-RECORD_SECONDS = 5
-
-audio1 = pyaudio.PyAudio()
-
-#camera = cv2.VideoCapture(0)
-
-#can use this to stream in the AI's deepfake output
-#how to send over sound?
-def gen_frames():
-	image_paths = ['./Resources/Images/fly_dog.jpg', './Resources/Images/jump_dog.jpg']
-	i = 0
-	while True:
-		# success, frame = camera.read() 
-		frame = cv2.imread(image_paths[i])
-		success = True
-		i += 1
-		if i >= 2:
-			i = 0
-
-		if not success:
-			break
-		else:
-			ret, buffer = cv2.imencode('.jpg', frame)
-			frame = buffer.tobytes()
-			#concatenates frames one by one
-			yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-def genHeader(sampleRate, bitsPerSample, channels):
-    datasize = 2000*10**6
-    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
-    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
-    o += bytes("WAVE",'ascii')                                              # (4byte) File type
-    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
-    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
-    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
-    o += (channels).to_bytes(2,'little')                                    # (2byte)
-    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
-    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
-    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
-    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
-    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
-    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
-    return o
-
-@app.route('/audio')
-def audio():
-    # start Recording
-    def sound():
-
-        CHUNK = 1024
-        sampleRate = 44100
-        bitsPerSample = 16
-        channels = 2
-        wav_header = genHeader(sampleRate, bitsPerSample, channels)
-
-        stream = audio1.open(format=FORMAT, channels=CHANNELS,
-                        rate=RATE, input=True,input_device_index=1,
-                        frames_per_buffer=CHUNK)
-        print("recording...")
-        #frames = []
-        first_run = True
-        while True:
-           if first_run:
-               data = wav_header + stream.read(CHUNK)
-               first_run = False
-           else:
-               data = stream.read(CHUNK)
-           yield(data)
-
-    return Response(sound())
-
-@app.route("/wav")
-def streamwav():
-    def generate():
-        with open("./Resources/Songs/out_loud.wav", "rb") as fwav:
-            data = fwav.read(1024)
-            while data:
-                yield data
-                data = fwav.read(1024)
-    return Response(generate(), mimetype="audio/x-wav")
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-#streaming support
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/training_data', methods=['POST'])
 def training_data():
